f47a55ee009a252f56102877325e623608f6647e
Fahima Islam (3fi) <islamff@ornl.gov>
2021-04-20 20:46:32 -0400

    added the kernel file

--- acc/arrowINtersection.py
+++ acc/arrowINtersection.py
@@ -1,8 +1,25 @@
 from numba import cuda
 import numpy as np
+import math
+import numba
 
-@cuda.jit
-def intersectRectangle(rx, ry, rz,vx, vy, vz,X, Y, t):
+
+
+@cuda.jit(device=True)
+def oneNeutronINtersectRectangle(rx, ry, rz,vx, vy, vz,X, Y):
+    t_pos = (0 - rz) / vz
+    r1x = rx + vx * t_pos
+    r1y = ry + vy * t_pos
+    bad = abs(r1x) > X / 2 or abs(r1y) > Y / 2
+    if bad:
+        t_pos= np.nan
+    return t_pos
+
+
+
+@cuda.jit()
+def intersectBox(arrow_array_position, arrow_array_direction, box, t):
+    # neutron_times = []
     # Thread id in a 1D block
     tx = cuda.threadIdx.x
     # Block id in a 1D grid
@@ -11,42 +28,75 @@ def intersectRectangle(rx, ry, rz,vx, vy, vz,X, Y, t):
     bw = cuda.blockDim.x
     # Compute flattened index inside the array
     pos = tx + ty * bw
-    if pos < t.size:  # Check array boundaries
-        t_pos = (0-rz[pos])/vz[pos]
-        r1x = rx[pos] + vx[pos] * t_pos
-        r1y = ry[pos] + vy[pos] * t_pos
-        # print (pos, t_pos, r1x, r1y, t.size)
-        if abs(r1x)<X[pos]/2 and abs(r1y)<Y[pos]/2:
-            t[pos]=t_pos
-        else:
-            t[pos] = np.nan
+
+    # calculate for each pos  (index of the neutrons) intersection which of the phases of one box
+    t[pos, 0] = np.nan
+    t[pos,1] = np.nan
+    if pos>arrow_array_direction.shape[0]:
+        return
+    start = arrow_array_position[pos]
+    direction = arrow_array_direction[pos]
+    x = start[0]
+    y = start[1]
+    z = start[2]
+    vx = direction[0]
+    vy = direction[1]
+    vz = direction[2]
+
+
+    X = box[0]
+    Y = box[1]
+    Z = box[2]
+    # t_store = np.empty(shape=len(t)+3)
+    t_cal = cuda.local.array(6, numba.float64)
+    for i in range(6):
+        t_cal[i] = np.nan
+    if vz != 0:
+        t_cal[0] = oneNeutronINtersectRectangle(x, y, z-Z / 2, vx, vy, vz, X, Y)
+        t_cal[1] = oneNeutronINtersectRectangle(x, y, z+Z / 2, vx, vy, vz, X, Y)
 
 
 
+    if vx != 0:
+        t_cal[2] = oneNeutronINtersectRectangle(y, z, x-X / 2, vy, vz, vx, Y, Z)
+        t_cal[3] = oneNeutronINtersectRectangle(y, z, x+X / 2, vy, vz, vx, Y, Z)
 
-# rx= np.array([0.,0.,0.])
-# ry= np.array([0.,0,0])
-# rz= np.array([1.,1,1])
-# vx= np.array([1.,0,-5])
-# vy= np.array([0.,0,0])
-# vz= np.array([-1.,-1,-1])
-# X=np.array([3.,0.5,3])
-# Y=np.array([1.,1,1])
-# t=np.zeros(rx.shape)
 
-array_size =100000000
-rx= np.zeros((array_size))
-ry= np.zeros((array_size))
-rz= np.ones((array_size))
-vx= np.ones((array_size))
-vy= np.zeros((array_size))
-vz= -np.ones((array_size))
-X=np.ones((array_size))*3
-Y=np.ones((array_size))
-t=np.zeros(rx.shape)
+    if vy != 0:
+        t_cal[4] =oneNeutronINtersectRectangle(z, x, y-Y / 2, vz, vx, vy, Z, X)
+        t_cal[5] =oneNeutronINtersectRectangle(z, x, y+Y / 2, vz, vx, vy, Z, X)
 
-threadsperblock = 32
-blockspergrid = (t.size + (threadsperblock - 1)) // threadsperblock
-intersectRectangle[blockspergrid, threadsperblock](rx, ry, rz,vx, vy, vz,X, Y,t)
+    for i in range(6):
+        if not math.isnan(t_cal[i]):
+            if math.isnan (t[pos,0]):
+                t[pos,0] = t_cal[i]
+            else:
+                t[pos,1] = t_cal[i]
 
-# print (t)
+    if t[pos,0] > t[pos,1]:
+        t[pos,0], t[pos,1] = t[pos,1], t[pos,0]
+
+
+
+def ArrowIntersector(arrow_array_position, arrow_array_direction, box, threadsperblock = 64):
+    '''
+
+    :param box: NamedTuple :
+    :param m_arrow:
+    :param threadsperblock:
+    :return:
+    '''
+
+    arrow_array_position_gpu = cuda.to_device(arrow_array_position)
+
+    arrow_array_direction_gpu = cuda.to_device(arrow_array_direction)
+
+    box_gpu = cuda.to_device(box)
+    t_gpu = cuda.device_array((len(arrow_array_position),2))
+
+    blockspergrid = (t_gpu.size + (threadsperblock - 1)) // threadsperblock
+    intersectBox[blockspergrid, threadsperblock](arrow_array_position_gpu, arrow_array_direction_gpu,
+                                                 box_gpu, t_gpu)
+    t = t_gpu.copy_to_host()
+    return t
+
---
+++ acc/test_arrowINtersection.py
@@ -0,0 +1,27 @@
+from arrowINtersection import ArrowIntersector
+# from numpy_arrow_box import ArrowIntersector
+import numpy as np
+from collections import namedtuple
+
+def test_arrowIntersector():
+    # box = box_type(edge_X=1, edge_Y=2, edge_Z=3)
+    # position = vector_3D(x=0, y=0, z=-5)
+    # direction = vector_3D(x=0, y=0, z=1)
+    # arrow = [arrow_type(position = position, direction = direction)]*10000
+
+    arrow_array_position = np.array([[0.,0.,-5.]]*10000)
+    # arrow_array_position = np.array([[-1, 0, 1.5]]) #given by andrei
+    # arrow_array_position = np.array([[0, 0, -5]])
+    arrow_array_direction = np.array([[0.,0.,1.]]*10000)
+    # arrow_array_direction = np.array([[1, 0, 0]]) #given by andrei
+    # arrow_array_direction = np.array([[0, 0, 1]])
+    # box = np.array([1,1,3]) #given by andrei
+    box = np.array([1., 2., 3.])
+    # print (arrow_array_position[0][0])
+
+
+    time = ArrowIntersector(arrow_array_position,arrow_array_direction,box)
+    assert(len(time) == 10000)
+    assert(len(time[20])==2)
+    assert (time[0][0] == 3.5)
+    assert (time[0][1] == 6.5)
