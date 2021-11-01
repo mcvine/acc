94cfca00b0b52a748c58a2071eeae7b439512938
Jiao Lin <jiao.lin@gmail.com>
2021-11-01 11:25:25 -0700

    added code for plotting:   tests/optics/test_guide_cpu.py

--- tests/optics/test_guide_cpu.py
+++ tests/optics/test_guide_cpu.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
-import os
+import os, shutil
 thisdir = os.path.dirname(__file__)
 from mcvine import run_script
+interactive = False
 
 def test():
     instr = os.path.join(thisdir, "guide_cpu_instrument.py")
     outdir = 'out.debug-guide_cpu_instrument'
+    if os.path.exists(outdir): shutil.rmtree(outdir)
     run_script.run1(instr, outdir, ncount=100000, overwrite_datafiles=True)
+    global interactive
+    if interactive:
+        from histogram import plot as plotHist
+        import histogram.hdf as hh
+        Ixy = hh.load(os.path.join(outdir, "Ixy.h5"))
+        plotHist(Ixy)
+        Ixdivx = hh.load(os.path.join(outdir, "Ixdivx.h5"))
+        plotHist(Ixdivx)
     return
 
 def main():
+    global interactive
+    interactive = True
     test()
     return
 
