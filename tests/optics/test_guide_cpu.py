ac6470dceab9b38c65336b246bceb617eeb5a8b5
Cole Kendrick <kendrickcj@ornl.gov>
2021-11-05 13:31:27 -0400

    Update cpu guide test to compare files

--- tests/optics/test_guide_cpu.py
+++ tests/optics/test_guide_cpu.py
@@ -2,6 +2,7 @@
 
 import os, shutil
 import histogram.hdf as hh
+import numpy as np
 thisdir = os.path.dirname(__file__)
 from mcvine import run_script
 interactive = False
@@ -23,11 +24,16 @@ def test():
     if os.path.exists(outdir): shutil.rmtree(outdir)
     run_script.run1(instr, outdir, ncount=num_neutrons, overwrite_datafiles=True)
 
-    # TODO: Compare output files
+    # Compare output files
     mcvine_Ixy = hh.load(os.path.join(mcvine_outdir, "Ixy.h5"))
     mcvine_Ixdivx = hh.load(os.path.join(mcvine_outdir, "Ixdivx.h5"))
     Ixy = hh.load(os.path.join(outdir, "Ixy.h5"))
     Ixdivx = hh.load(os.path.join(outdir, "Ixdivx.h5"))
+
+    assert mcvine_Ixy.shape() == Ixy.shape()
+    assert mcvine_Ixdivx.shape() == Ixdivx.shape()
+    assert np.allclose(mcvine_Ixy.data().storage(), Ixy.data().storage())
+    assert np.allclose(mcvine_Ixdivx.data().storage(), Ixdivx.data().storage())
 
     global interactive
     if interactive:
