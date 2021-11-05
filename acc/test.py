25935b10ee891744d01d463d499da695e0bb6427
Jiao Lin <jiao.lin@gmail.com>
2021-11-05 15:32:07 -0700

    check NUMBA_ENABLE_CUDASIM:   acc/test.py

--- acc/test.py
+++ acc/test.py
@@ -1,4 +1,8 @@
 import os
-USE_CUDA = os.environ.get('USE_CUDA', 'true') in ['true', '1', 'yes']
-import numba.cuda
-USE_CUDA = USE_CUDA and numba.cuda.is_available()
+USE_CUDASIM = os.environ.get('NUMBA_ENABLE_CUDASIM', 'false') in ['true', '1', 'yes']
+if not USE_CUDASIM:
+    USE_CUDA = os.environ.get('USE_CUDA', 'true') in ['true', '1', 'yes']
+    import numba.cuda
+    USE_CUDA = USE_CUDA and numba.cuda.is_available()
+else:
+    USE_CUDA = False
