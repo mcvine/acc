77963cd05c068d1d45170d2b76902fe18df6d07e
Jiao Lin <jiao.lin@gmail.com>
2022-02-26 21:05:35 -0800

    check guide instead of guide_baseline:   tests/components/optics/check_guide_speed.py

--- tests/components/optics/check_guide_speed.py
+++ tests/components/optics/check_guide_speed.py
@@ -11,8 +11,8 @@ def test():
     from mcni.neutron_storage import load
     neutron_file = 'data/before_guide-n1e8.mcv'
     neutrons = load(neutron_file)
-    from mcvine.acc.components.optics import guide_baseline
-    g = guide_baseline.Guide(
+    from mcvine.acc.components.optics import guide
+    g = guide.Guide(
         name = 'guide',
         w1=0.035, h1=0.035, w2=0.035, h2=0.035, l=10,
         R0=0.99, Qc=0.0219, alpha=6.07, m=3, W=0.003,
