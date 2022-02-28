9614508853151b23dae03da3e1a07fbeea13941f
Jiao Lin <jiao.lin@gmail.com>
2022-02-27 20:42:04 -0800

    added pytest decorator:   tests/test_run_script.py

--- tests/test_run_script.py
+++ tests/test_run_script.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
-import os
+import os, pytest
 thisdir = os.path.abspath(os.path.dirname(__file__))
+from mcvine.acc import test
 
 script = os.path.join(thisdir, 'acc_sgm_instrument.py')
 workdir = 'out.acc_sgm'
-ncount = int(1e5)
+ncount = int(1e8)
 
 from mcvine.acc import run_script
-run_script.compile(script)
-run_script.run(script, 'out.test_run_script', ncount=1e8)
+
+@pytest.mark.skipif(not test.USE_CUDA, reason='No CUDA')
+def test_compile():
+    run_script.compile(script)
+    return
+
+@pytest.mark.skipif(not test.USE_CUDA, reason='No CUDA')
+def test_run():
+    run_script.run(script, workdir, ncount=ncount)
+    return
+
+
+def main():
+    test_run()
+    return
+
+if __name__ == '__main__': main()
