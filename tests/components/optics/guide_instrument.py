ac15d6bd89382bb04bd10bb903d53709af6ebcf5
Mark Carroll <carrollmt@ornl.gov>
2022-04-12 15:44:59 -0400

    Adapt some instruments and their tests to use new utilities.
    - arm
    - beamstop
    - guide
    - slit

--- tests/components/optics/guide_instrument.py
+++ tests/components/optics/guide_instrument.py
@@ -1,57 +1,28 @@
 #!/usr/bin/env python
 
-import mcvine, mcvine.components
-mc = mcvine.components
-from mcni import rng_seed
-def seed(): return 0
-rng_seed.seed = seed
+from instrument_factory import construct
+
 
 def instrument(
-        guide_mod=None, guide_factory=None,
-        save_neutrons_before_guide=False, save_neutrons_after_guide=False,
-):
-    instrument = mcvine.instrument()
-    source = mc.sources.Source_simple(
-        name = 'source',
-        radius = 0., width = 0.03, height = 0.03, dist = 1.,
-        xw = 0.035, yh = 0.035,
-        Lambda0 = 10., dLambda = 9.5,
-    )
-    instrument.append(source, position=(0,0,0.))
-    if save_neutrons_before_guide:
-        before_guide = mc.monitors.NeutronToStorage(name='before_guide', path='before_guide.mcv')
-        instrument.append(before_guide, position=(0, 0, 1.))
-    if guide_mod:
+        guide_mod=None, guide_factory=None, is_acc=False):
+    if guide_factory:
+        import mcvine.components
+        factory = eval(guide_factory)
+    elif guide_mod:
         import importlib
-        mod = importlib.import_module(guide_mod)
-        # assume factory name is "Guide" in the given module
-        guide_factory = mod.Guide
-    elif guide_factory:
-        guide_factory = eval(guide_factory)
-    acc_guide = guide_factory(
-        name = 'guide',
-        w1=0.035, h1=0.035, w2=0.035, h2=0.035, l=10,
-        R0=0.99, Qc=0.0219, alpha=6.07, m=3, W=0.003,
+        guide_module = importlib.import_module(guide_mod)
+        factory = guide_module.Guide
+    elif is_acc:
+        from mcvine.acc.components.optics.guide import Guide
+        factory = Guide
+    else:
+        import mcvine.components
+        factory = mcvine.components.optics.Guide
+
+    guide_length = 10.
+    target = factory(
+        name='guide',
+        w1=0.035, h1=0.035, w2=0.035, h2=0.035, l=guide_length,
+        R0=0.99, Qc=0.0219, alpha=6.07, m=3, W=0.003
     )
-    instrument.append(acc_guide, position=(0, 0, 1.))
-    if save_neutrons_after_guide:
-        after_guide = mc.monitors.NeutronToStorage(name='after_guide', path='after_guide.mcv')
-        instrument.append(after_guide, position=(0, 0, 11.))
-    Ixy = mc.monitors.PSD_monitor(
-        name = 'Ixy', nx=250, ny=250, filename="Ixy.dat", xwidth=0.08, yheight=0.08,
-        restore_neutron=True
-    )
-    instrument.append(Ixy, position=(0,0,12))
-    Ixdivx = mc.monitors.DivPos_monitor(
-        name = 'Ixdivx',
-        npos=250, ndiv=250, filename="Ixdivx.dat", xwidth=0.08, yheight=0.08,
-        restore_neutron=True
-    )
-    instrument.append(Ixdivx, position=(0,0,12))
-    Iydivy = mc.monitors.DivPos_monitor(
-        name = 'Iydivy',
-        npos=250, ndiv=250, filename="Iydivy.dat", xwidth=0.08, yheight=0.08,
-        restore_neutron=True
-    )
-    instrument.append(Iydivy, position=(0,0,12), orientation=(0, 0, 90))
-    return instrument
+    return construct(target, guide_length)
--- tests/components/optics/slit_instrument.py
+++ tests/components/optics/slit_instrument.py
@@ -1,51 +1,18 @@
 #!/usr/bin/env python
 
-import mcvine, mcvine.components
-mc = mcvine.components
-from mcni import rng_seed
-
-rng_seed.seed = lambda: 0
+from instrument_factory import construct
 
 
-def instrument(
-        module=None, factory=None,
-):
-    instrument = mcvine.instrument()
-    source = mc.sources.Source_simple(
-        name='source',
-        radius=0., width=0.03, height=0.03, dist=1.,
-        xw=0.035, yh=0.035,
-        Lambda0=10., dLambda=9.5
-    )
-    instrument.append(source, position=(0, 0, 0))
-    if module:
-        import importlib
-        mod = importlib.import_module(module)
-        # assume factory name is "Slit" in the given module
-        factory = mod.Slit
-    elif factory:
-        factory = eval(factory)
-    acc_slit = factory(
+def instrument(is_acc):
+    if is_acc:
+        from mcvine.acc.components.optics.slit import Slit
+        factory = Slit
+    else:
+        import mcvine.components
+        factory = mcvine.components.optics.Slit
+
+    target = factory(
         name='slit',
         width=0.02, height=0.01, cut=0.001
     )
-    instrument.append(acc_slit, position=(0, 0, 1))
-    Ixy = mc.monitors.PSD_monitor(
-        name='Ixy',
-        nx=250, ny=250, filename="Ixy.dat", xwidth=0.08, yheight=0.08,
-        restore_neutron=True
-    )
-    instrument.append(Ixy, position=(0, 0, 1.5))
-    Ixdivx = mc.monitors.DivPos_monitor(
-        name='Ixdivx',
-        npos=250, ndiv=250, filename="Ixdivx.dat", xwidth=0.08, yheight=0.08,
-        restore_neutron=True
-    )
-    instrument.append(Ixdivx, position=(0, 0, 1.5))
-    Iydivy = mc.monitors.DivPos_monitor(
-        name='Iydivy',
-        npos=250, ndiv=250, filename="Iydivy.dat", xwidth=0.08, yheight=0.08,
-        restore_neutron=True
-    )
-    instrument.append(Iydivy, position=(0, 0, 1.5), orientation=(0, 0, 90))
-    return instrument
+    return construct(target, 0.)
