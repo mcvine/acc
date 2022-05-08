80891fa169c1936e4da0f6ebdcbd8af8351a0afb
Jiao Lin <jiao.lin@gmail.com>
2022-05-08 06:09:30 -0700

    added an implementation that replicates the current CPU implementation:   kernels/isotropic.py

--- acc/kernels/isotropic.py
+++ acc/kernels/isotropic.py
@@ -19,3 +19,21 @@ def scatter(threadindex, rng_states, neutron):
     neutron[3:6] = vx,vy,vz
     return
 
+
+@cuda.jit(device=True)
+def scatter(threadindex, rng_states, neutron):
+    # randomly pick direction
+    theta = xoroshiro128p_uniform_float32(rng_states, threadindex)*pi
+    phi = xoroshiro128p_uniform_float32(rng_states, threadindex)*(2*pi)
+    cos_t, sin_t = cos(theta), sin(theta)
+    sin_p, cos_p = sin(phi), cos(phi)
+    # compute velocity
+    vx, vy, vz = neutron[3:6]
+    vi = sqrt(vx*vx+vy*vy+vz*vz)
+    vx = vi*sin_t*cos_p
+    vy = vi*sin_t*sin_p
+    vz = vi*cos_t
+    neutron[3:6] = vx,vy,vz
+    neutron[-1] *= sin_t
+    return
+
