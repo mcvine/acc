3a38178848bb218f675bcf65a6d5b2d35ddc7abe
Jiao Lin <jiao.lin@gmail.com>
2022-06-27 12:50:45 -0400

    more test cases:  tests/geometry/test_oncylinder.py

--- tests/geometry/test_oncylinder.py
+++ tests/geometry/test_oncylinder.py
@@ -10,7 +10,18 @@ from mcvine.acc.geometry import oncylinder, location
 # device functions can be tested with CUDASIM only
 @pytest.mark.skipif(not test.USE_CUDASIM, reason='no CUDASIM')
 def test_cu_device_locate_wrt_cylinder():
-    assert oncylinder.cu_device_locate_wrt_cylinder(0,0,0, 0.02, 0.01)==location.inside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0,0, 0.02, 0.04)==location.inside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0.01,0,0, 0.02, 0.04)==location.inside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0.02,0,0, 0.02, 0.04)==location.onborder
+    assert oncylinder.cu_device_locate_wrt_cylinder(0.03,0,0, 0.02, 0.04)==location.outside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0.01,0, 0.02, 0.04)==location.inside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0.019999,0, 0.02, 0.04)==location.inside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0.02,0, 0.02, 0.04)==location.onborder
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0.03,0, 0.02, 0.04)==location.outside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0.02001,0, 0.02, 0.04)==location.outside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0,0.02, 0.02, 0.04)==location.onborder
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0,0.019999, 0.02, 0.04)==location.inside
+    assert oncylinder.cu_device_locate_wrt_cylinder(0,0,0.020001, 0.02, 0.04)==location.outside
     return
 
 def main():
