d2cd3a1df50202b66e64bb88b09f2411dbd84371
Jiao Lin <jiao.lin@gmail.com>
2022-07-01 20:12:23 -0400

    added acc.components.samples.getAbsScttCoeffs

--- acc/components/samples/__init__.py
+++ acc/components/samples/__init__.py
@@ -23,3 +23,29 @@ def loadScattererComposite(sampleassembly_xml):
 def loadFirstHomogeneousScatterer(sampleassembly_xml):
     sc = loadScattererComposite(sampleassembly_xml)
     return sc.elements()[0]
+
+def getAbsScttCoeffs(kernel):
+    abs = kernel.absorption_coefficient
+    sctt = kernel.scattering_coefficient
+
+    if abs is None or sctt is None:
+        #need to get cross section from sample assembly representation
+        # svn://danse.us/inelastic/sample/.../sampleassembly
+        #origin is a node in the sample assembly representation
+        #
+        #scatterer_origin is assigned to kernel when a kernel is
+        #constructed from kernel xml.
+        #see sampleassembly_support.SampleAssembly2CompositeScatterer for details.
+        origin = kernel.scatterer_origin
+        from sampleassembly import cross_sections
+        abs, inc, coh = cross_sections( origin )
+        sctt = inc + coh
+        pass
+
+    abs, sctt = _units_remover.remove_unit( (abs, sctt), 1./units.length.meter )
+    return abs, sctt
+
+from mccomposite.units_utils import UnitsRemover
+from mccomposite import units
+_units_remover = UnitsRemover(
+    length_unit = units.length.meter, angle_unit = units.angle.degree)
