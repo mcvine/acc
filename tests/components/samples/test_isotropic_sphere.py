36c05d3c9f89a03ed43dca450f8a5e22d9e0a804
Jiao Lin <jiao.lin@gmail.com>
2022-07-02 08:27:30 -0400

    walltime comparison for "isotropic sphere"

--- tests/components/samples/test_isotropic_sphere.py
+++ tests/components/samples/test_isotropic_sphere.py
@@ -12,9 +12,10 @@ def test1():
     instr = os.path.join(thisdir, "isotropic_sphere_instrument.py")
     outdir = 'out.isotropic_sphere'
     if os.path.exists(outdir): shutil.rmtree(outdir)
+    ncount = 1e5
     run_script.run1(
         instr, outdir,
-        ncount=1e5, buffer_size=int(1e5),
+        ncount=ncount, buffer_size=int(ncount),
     )
     return
 
@@ -40,9 +41,9 @@ def test_compare_mcvine(num_neutrons=int(1e7), debug=False, interactive=False):
 def main():
     import journal
     journal.info("instrument").activate()
-    test_compare_mcvine(num_neutrons=int(1e7), interactive=True)
-    # test_compare_mcvine(num_neutrons=int(100), interactive=True, debug=True)
     # test1()
+    # test_compare_mcvine(num_neutrons=int(100), interactive=True, debug=True)
+    test_compare_mcvine(num_neutrons=int(1e7), interactive=True)
     return
 
 
