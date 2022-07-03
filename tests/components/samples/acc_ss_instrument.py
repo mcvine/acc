499b6eedf959de670ce197b234cb85ed91c65191
Jiao Lin <jiao.lin@gmail.com>
2022-07-03 12:23:42 -0400

    added kargs "monitor_factory" and "z_sample"; modified source parameters to match other test instruments:   tests/components/samples/acc_ss_instrument.py

--- tests/components/samples/acc_ss_instrument.py
+++ tests/components/samples/acc_ss_instrument.py
@@ -5,19 +5,23 @@ from mcvine.acc.components.sources.source_simple import Source_simple
 from HSS_isotropic_sphere import HSS
 thisdir = os.path.dirname(__file__)
 
-def instrument():
+def instrument(monitor_factory=None, z_sample=1.):
     instrument = mcvine.instrument()
 
     source = Source_simple(
         'src',
-        radius = 0., width = 0.03, height = 0.03, dist = 1.,
-        xw = 0.035, yh = 0.035,
+        radius = 0., width = 0.01, height = 0.01, dist = 1.,
+        xw = 0.008, yh = 0.008,
         Lambda0 = 10., dLambda = 9.5, E0=0., dE=0.0,
         flux=1, gauss=0
     )
     instrument.append(source, position=(0,0,0.))
 
     sample = HSS('sample')
-    instrument.append(sample, position=(0,0,1.))
+    instrument.append(sample, position=(0,0,z_sample))
+
+    if monitor_factory is not None:
+        monitor = monitor_factory()
+        instrument.append(monitor, position=(0,0,z_sample))
 
     return instrument
