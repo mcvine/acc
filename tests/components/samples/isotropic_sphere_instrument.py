a25f2a740a20594ac0cfa75587daeac22f9b27a3
Jiao Lin <jiao.lin@gmail.com>
2022-07-04 11:50:00 -0400

    removed unused code: tests/components/samples/isotropic_sphere_instrument.py

--- tests/components/samples/isotropic_sphere_instrument.py
+++ tests/components/samples/isotropic_sphere_instrument.py
@@ -7,7 +7,6 @@ from test_sample_instrument_factory import construct
 
 def instrument(is_acc=True):
     if is_acc:
-        from mcvine.acc.components.samples.isotropic_box import IsotropicBox as factory
         from HSS_isotropic_sphere import HSS
         target = HSS(name='sample')
     else:
