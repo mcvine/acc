ac9e0d369f5259bef5096ff9db2a319d9e3c1899
Jiao Lin <jiao.lin@gmail.com>
2022-11-04 09:49:27 -0400

    added more test cases and check against expected results:   tests/geometry/test_propagation.py

--- tests/geometry/test_propagation.py
+++ tests/geometry/test_propagation.py
@@ -16,14 +16,26 @@ thisdir = os.path.dirname(__file__)
 def test_propagate_out():
     parsed = parse_file(os.path.join(thisdir, 'hollowcylinder_example1.xml'))
     shape = parsed[0]
-    print(shape)
+    # print(shape)
     intersect = arrow_intersect.arrow_intersect_func_factory.render(shape)
     locate = arrow_intersect.locate_func_factory.render(shape)
     forward_intersect, propagate_out, propagate_to_next_incident_surface, propagate_to_next_exiting_surface = \
         makePropagateMethods(intersect, locate)
-    neutron = np.array([0.,0.,0., 1.,0.,0., 0.,0., 0., 1.])
-    propagate_out(neutron)
-    print(neutron)
+    neutrons = np.array([
+        [0.,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.01,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.015,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.,0.,0., 0.,0.,1., 0.,0., 0., 1.],
+    ])
+    expected = np.array([
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.02, 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.01, 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.005, 1.],
+        [0.,0.,0., 0.,0.,1., 0.,0., 0., 1.],
+    ])
+    for neutron, out in zip(neutrons, expected):
+        propagate_out(neutron)
+        assert np.allclose(neutron, out), f"{neutron} != {out}"
     return
 
 def main():
