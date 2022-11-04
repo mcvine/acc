452c444adf197cc296254698ed309b38767939ef
Jiao Lin <jiao.lin@gmail.com>
2022-11-04 11:08:47 -0400

    added tests for `test_propagate_to_next_exiting_surface`

--- tests/geometry/test_propagation.py
+++ tests/geometry/test_propagation.py
@@ -89,9 +89,47 @@ def test_propagate_to_next_incident_surface():
             propagate_to_next_incident_surface(neutron)
     return
 
+@pytest.mark.skipif(not test.USE_CUDASIM, reason='no CUDASIM')
+def test_propagate_to_next_exiting_surface():
+    parsed = parse_file(os.path.join(thisdir, 'hollowcylinder_example1.xml'))
+    shape = parsed[0]
+    # print(shape)
+    intersect = arrow_intersect.arrow_intersect_func_factory.render(shape)
+    locate = arrow_intersect.locate_func_factory.render(shape)
+    forward_intersect, propagate_out, propagate_to_next_incident_surface, propagate_to_next_exiting_surface = \
+        makePropagateMethods(intersect, locate)
+    neutrons = np.array([
+        [-1.,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [-0.025,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [-0.02,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [-0.015,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [-0.01,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.01,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.015,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [0.025,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [1.,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+     ])
+    expected = np.array([
+        [-0.01,0.,0., 1.,0.,0., 0.,0., 0.99, 1.],
+        [-0.01,0.,0., 1.,0.,0., 0.,0., 0.015, 1.],
+        [-0.01,0.,0., 1.,0.,0., 0.,0., 0.01, 1.],
+        [-0.01,0.,0., 1.,0.,0., 0.,0., 0.005, 1.],
+        [-0.01,0.,0., 1.,0.,0., 0.,0., 0.01, 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.02, 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.01, 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.005, 1.],
+        [0.02,0.,0., 1.,0.,0., 0.,0., 0.0, 1.],
+        [0.025,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+        [1.,0.,0., 1.,0.,0., 0.,0., 0., 1.],
+    ])
+    return
+
 def main():
     test_propagate_out()
     test_propagate_to_next_incident_surface()
+    test_propagate_to_next_exiting_surface()
     return
 
 if __name__ == '__main__': main()
