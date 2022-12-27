fcb26610bb7fa2a0442bb975ea300a5168946b03
Jiao Lin <jiao.lin@gmail.com>
2022-12-27 14:31:49 -0800

    test passed:   tests/components/samples/test_ss_UN_block.py

--- tests/components/samples/ss_UN_block_instrument.py
+++ tests/components/samples/ss_UN_block_instrument.py
@@ -5,13 +5,13 @@ thisdir = os.path.dirname(__file__)
 
 from test_powdersqesample_instrument_factory import construct, Builder as BuilderBase
 
-Ei = 400.0
+Ei = 500.0
 class Builder(BuilderBase):
     def addIQEMonitor(self):
         return super(Builder, self).addIQEMonitor(
             Ei=Ei,
-            Qmin=0., Qmax=20.0, nQ = 200,
-            Emin=-200.0, Emax=600.0, nE = 120,
+            Qmin=0., Qmax=30.0, nQ = 300,
+            Emin=-100.0, Emax=400.0, nE = 200,
         )
 
 def instrument(is_acc=True):
@@ -22,7 +22,7 @@ def instrument(is_acc=True):
         import mcvine.components as mc
         xml=os.path.join(thisdir, "sampleassemblies", "UN", "sampleassembly.xml")
         target = mc.samples.SampleAssemblyFromXml("sample", xml)
-    source_params = dict(E0 = Ei, dE=0.05*Ei, Lambda0=0, dLambda=0.)
+    source_params = dict(E0 = Ei, dE=0.01*Ei, Lambda0=0, dLambda=0.)
     return construct(
         target, size=0.,
         source_params=source_params, monitors=['IQE'],
