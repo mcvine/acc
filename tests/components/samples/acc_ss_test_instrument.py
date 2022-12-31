26c36d1c7ea1f0e050e858ef618344d2a811bb85
Jiao Lin <jiao.lin@gmail.com>
2022-12-30 16:33:00 -0800

    added source_factory and sample_factory as kargs: tests/components/symples/acc_ss_test_instrument.py

--- tests/components/samples/acc_ss_test_instrument.py
+++ tests/components/samples/acc_ss_test_instrument.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 
 import os, mcvine
-from mcvine.acc.components.sources.source_simple import Source_simple
-from HSS_isotropic_hollowcylinder import HSS
 thisdir = os.path.dirname(__file__)
 
-def instrument(monitor_factory=None, z_sample=2.0):
+def instrument(sample_factory=None, monitor_factory=None, z_sample=2.0, source_factory=None):
     instrument = mcvine.instrument()
-
-    source = Source_simple(
-        'src',
-        radius = 0., width = 0.01, height = 0.01, dist = 1.,
-        xw = 0.008, yh = 0.008,
-        Lambda0 = 10., dLambda = 9.5, E0=0., dE=0.0,
-        flux=1, gauss=0
-    )
+    if source_factory is None:
+        from mcvine.acc.components.sources.source_simple import Source_simple
+        source = Source_simple(
+            'src',
+            radius = 0., width = 0.01, height = 0.01, dist = 1.,
+            xw = 0.008, yh = 0.008,
+            Lambda0 = 10., dLambda = 9.5, E0=0., dE=0.0,
+            flux=1, gauss=0
+        )
+    else:
+        source = source_factory()
     instrument.append(source, position=(0,0,0.))
 
-    sample = HSS('sample')
+    if sample_factory is None:
+        from HSS_isotropic_hollowcylinder import HSS
+        sample = HMS('sample')
+    else:
+        sample = sample_factory()
     instrument.append(sample, position=(0,0,z_sample))
 
     if monitor_factory is not None:
