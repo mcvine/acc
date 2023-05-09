05ad8a0df03d54b6ea54828c811cd9301d5baac6
Jiao Lin <jiao.lin@gmail.com>
2023-05-09 07:48:49 -0700

    added a real CUDA test:   tests/geometry/test_composite_shape.py

--- tests/geometry/test_composite_shape.py
+++ tests/geometry/test_composite_shape.py
@@ -64,3 +64,33 @@ def test_find_1st_hit(ray_tracing_methods):
     assert find_1st_hit(0.05,0.,0., 1.,0.,0.) == 1
     assert find_1st_hit(0.15,0.,0., 1.,0.,0.) == 0
     assert find_1st_hit(0.25,0.,0., 1.,0.,0.) == -1
+
+@pytest.mark.skipif(not test.USE_CUDA, reason='no CUDA')
+def test_cuda(ray_tracing_methods):
+    find_1st_hit = make_find_1st_hit(**ray_tracing_methods)
+    @cuda.jit
+    def find_1st_hit_kernel(neutrons, out, n_neutrons_per_thread):
+        N = len(neutrons)
+        thread_index = cuda.grid(1)
+        start_index = thread_index*n_neutrons_per_thread
+        end_index = min(start_index+n_neutrons_per_thread, N)
+        for i in range(start_index, end_index):
+            neutron = neutrons[i]
+            x,y,z, vx,vy,vz = neutron[:6]
+            out[i] = find_1st_hit(x,y,z, vx,vy,vz)
+    def run(neutrons, first_hit):
+        N = len(neutrons)
+        threads_per_block = 128
+        ntotthreads = int(1e5)
+        nblocks = math.ceil(ntotthreads / threads_per_block)
+        actual_nthreads = threads_per_block * nblocks
+        n_neutrons_per_thread = math.ceil(N / actual_nthreads)
+        find_1st_hit_kernel[nblocks, threads_per_block](neutrons, first_hit, n_neutrons_per_thread)
+    N = int(100)
+    neutrons = np.zeros((N, 10))
+    neutrons[:, :3] = np.random.random((N, 3)) * 0.1
+    neutrons[:, 3:6] = np.random.random((N, 3)) * 1000
+    first_hit = np.zeros(N, dtype=int)
+    run(neutrons, first_hit)
+    print(first_hit)
+    return
