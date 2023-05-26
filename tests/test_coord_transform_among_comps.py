8de206cc55bf8bd5a30ef087db773c23e6869d40
Jiao Lin <jiao.lin@gmail.com>
2023-05-25 18:38:20 -0700

    a bit of inline doc:   tests/test_coord_transform_among_comps.py

--- tests/test_coord_transform_among_comps.py
+++ tests/test_coord_transform_among_comps.py
@@ -8,7 +8,7 @@ from mcvine.acc.components.optics.arm import Arm
 from mcvine.acc.run_script import calcTransformations
 from mcvine.acc.neutron import abs2rel
 
-def createInstrument():
+def createTestInstrument():
     instrument = mcvine.instrument()
     comp1 = Arm('comp1')
     instrument.append(comp1, position=(0,0,0.))
@@ -22,23 +22,25 @@ def createInstrument():
 
 @pytest.mark.skipif(not test.USE_CUDASIM, reason='No CUDASIM')
 def test():
-    instrument = createInstrument()
+    instrument = createTestInstrument()
     offsets, rotmats = calcTransformations(instrument)
+    assert len(rotmats) == len(offsets) == len(instrument.components)-1
+    # tmp arrays required by abs2rel
     tmp_position = np.array([0., 0., 0.])
     tmp_velocity = np.array([0., 0., 0.])
-    # translation only
+    # translation only between comp1 and comp2
     position = np.array([0., 0., 0.])
     velocity = np.array([0., 0., 1.])
     abs2rel(position, velocity, rotmats[0], offsets[0], tmp_position, tmp_velocity)
     assert np.allclose(position, [0,0,-1])
     assert np.allclose(velocity, [0,0,1])
-    # rotation only
+    # rotation only between comp2 and comp3
     position = np.array([0., 0., -1.])
     velocity = np.array([0., 0., 1.])
     abs2rel(position, velocity, rotmats[1], offsets[1], tmp_position, tmp_velocity)
     assert np.allclose(position, [1,0,0])
     assert np.allclose(velocity, [-1,0,0])
-    # translation and rotation
+    # translation and rotation between comp3 and comp4 (indirectly thru comp2)
     position = np.array([0., 0., 0])
     velocity = np.array([0., 0., 1.])
     abs2rel(position, velocity, rotmats[2], offsets[2], tmp_position, tmp_velocity)
