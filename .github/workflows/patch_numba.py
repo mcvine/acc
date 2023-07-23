b215d237714f982071b7f389de2e9a332e74180d
Jiao Lin <jiao.lin@gmail.com>
2023-07-22 22:12:13 -0700

    fixed a bug -- need abs path of patch file when patching:   .github/workflows/patch_numba.py

--- .github/workflows/patch_numba.py
+++ .github/workflows/patch_numba.py
@@ -4,7 +4,7 @@ import numba
 this_dir = os.path.dirname(__file__)
 numba_dir = os.path.dirname(numba.__file__)
 
-patch_file = os.path.join(this_dir, "numba-0.53.1.patch")
+patch_file = os.path.abspath(os.path.join(this_dir, "numba-0.53.1.patch"))
 
 cmd = f"patch -p1 < {patch_file}"
 rt = sp.call(cmd, shell=True, cwd=numba_dir)
