b49da7ef2df5d59723ba189cdfacda44ade191eb
Jiao Lin <jiao.lin@gmail.com>
2023-07-26 07:21:32 -0700

    explore patching numba 0.56.4 for using reference for arrays in closure patch file:   .github/workflows/numba-0.56.4.patch script to patch numba:   .github/workflows/patch_numba.py test case:   tests/test_array_in_closure.py

---
+++ tests/test_array_in_closure.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# copied from
+# https://github.com/numba/numba/issues/9084
+from numba import cuda
+import numpy as np
+
+N = 100000
+# closed_array = np.ones(N)
+closed_array = cuda.to_device(np.ones(N))
+
+@cuda.jit(cache=True)
+def kernel(r, x):
+    r[0] = closed_array[x]
+
+r = np.zeros(1)
+
+kernel[1, 1](r, 2)
+print(r[0])
+
