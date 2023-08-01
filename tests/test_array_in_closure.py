66125b753ed63d70a9faed5b3da7af38d33fa9ae
Cole Kendrick <kendrickcj@ornl.gov>
2023-08-01 13:01:05 -0400

    Update test_array_in_closure to use a proper pytest

--- tests/test_array_in_closure.py
+++ tests/test_array_in_closure.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
-# copied from
-# https://github.com/numba/numba/issues/9084
+import pytest
 from numba import cuda
 import numpy as np
 
-N = 100000
-# closed_array = np.ones(N)
-closed_array = cuda.to_device(np.ones(N))
+from mcvine.acc import test
 
-@cuda.jit()
-def kernel(r, x):
-    r[0] = closed_array[x]
 
-r = np.zeros(1)
+@pytest.mark.skipif(not test.USE_CUDA, reason='No CUDA')
+def test_array_in_closure():
+    # Simple test to check that Numba doesn't make a const copy of a global array
+    # copied from
+    # https://github.com/numba/numba/issues/9084
+    # NOTE: this test will currently fail on Numba versions without the 
+    #  patch from the linked issue above. 
+    #  This patch is included in this repo at: .github/workflows/numba-0.56.4.patch
 
-kernel[1, 1](r, 2)
-print(r[0])
+    N = 100000
+    # closed_array = np.ones(N)
+    closed_array = cuda.to_device(np.ones(N))
 
+    @cuda.jit(cache=False)
+    def kernel(r, x):
+        r[0] = closed_array[x]
+
+    r = np.zeros(1)
+
+    kernel[1, 1](r, 2)
+    print(r[0])
+
+    assert r[0] == 1.0
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
