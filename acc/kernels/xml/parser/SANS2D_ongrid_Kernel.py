886341f67adc492c11a00f604f8d6ca032260240
Cole Kendrick <kendrickcj@ornl.gov>
2023-09-07 09:41:40 -0400

    Use Qx and Qy data from H5 file

--- acc/kernels/xml/parser/SANS2D_ongrid_Kernel.py
+++ acc/kernels/xml/parser/SANS2D_ongrid_Kernel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
 
+import numpy as np
 from .KernelNode import KernelNode as base, debug
 
 class SANS2D_ongrid_Kernel(base):
@@ -15,16 +16,33 @@ class SANS2D_ongrid_Kernel(base):
 
         # check if S_QxQy is a .npy or .h5 file
         if kwds['S_QxQy'].endswith(".npy"):
-            import numpy as np
             S_QxQy = np.load( kwds['S_QxQy'] )
         elif kwds['S_QxQy'].endswith(".h5"):
             import h5py
             input_file = h5py.File(kwds['S_QxQy'], 'r')
-            # Load the intensity dataset "I" from file
-            if "I" not in input_file.keys():
-                raise RuntimeError("Error loading intensity from S_QxQy file, could not find dataset 'I'")
-            S_QxQy = input_file["I"][()]
+
+            # check that the file contains the necessary data
+            datasets = ['I', 'qx', 'qy']
+            for dataset in datasets:
+                if dataset not in input_file.keys():
+                    raise RuntimeError(
+                        "Error loading from S_QxQy file, could not find dataset '{}'".format(dataset))
+
+            # Load the intensity dataset 'I', and 'qx', 'qy' from file
+            S_QxQy = input_file['I'][()]
+            Qx = input_file['qx'][()]
+            Qy = input_file['qy'][()]
             input_file.close()
+
+            # make sure the Qx/Qy arrays match the intensity size
+            assert S_QxQy.shape[1] == len(Qx)
+            assert S_QxQy.shape[0] == len(Qy)
+
+            # overwrite the Qx,Qy min and max parameters from the input file
+            Qx_min = np.min(Qx)
+            Qx_max = np.max(Qx)
+            Qy_min = np.min(Qy)
+            Qy_max = np.max(Qy)
         else:
             raise RuntimeError("Unrecognized file type for S_QxQy. Expected .npy or .h5")
 
