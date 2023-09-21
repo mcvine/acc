68eb188d225dc7c3d729c77155b9b29b2e47e801
Jiao Lin <jiao.lin@gmail.com>
2023-09-20 23:03:30 -0700

    minor

--- tests/instruments/VERDI/test_verdi_cpu.py
+++ tests/instruments/VERDI/test_verdi_cpu.py
@@ -17,7 +17,7 @@ def test_run1():
         use_gpu=False, overwrite_datafiles=True)
     return
 
-@pytest.mark.skip(reason="debugging")
+@pytest.mark.skip(reason="failed to run at github CI with pytest. works when run without pytest")
 def test_run_mpi():
     run_script.run_mpi(
         script, workdir=workdir,
