aa138c0b88035d63a7d6b95ffc3cb16e39ddfc92
Jiao Lin <jiao.lin@gmail.com>
2024-01-21 10:39:11 -0800

    minor - fix path:   tests/components/optics/test_offio.py

--- tests/components/optics/test_offio.py
+++ tests/components/optics/test_offio.py
@@ -9,6 +9,6 @@ from mcvine.acc import test
 thisdir = os.path.dirname(__file__)
 
 def test_load():
-    path = './data/chess-guide-example/ST17_10_1.off'
+    path = os.path.join(thisdir, './data/chess-guide-example/ST17_10_1.off')
     vertices, faces = offio.load(path)
     assert faces.shape == (4,4)
